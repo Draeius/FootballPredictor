e1a63abf2cbd0a91e76185bb2c88c6af4007cb1a
Draeius <matthias@woelk-web.de>
2020-05-20 18:34:09 +0200

    delete util

--- util.py
+++
@@ -1,9 +0,0 @@
-
-from itertools import permutations
-from typing import List
-
-def permutateName(nameArray):
-    """
-    permutates the given name
-    """
-    return list(permutations(nameArray.split(" ")))
